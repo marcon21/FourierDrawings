6bf83a11a66ba1e83976546294eacdb043111846
marcon21 <marcon859@gmail.com>
2020-10-23 13:14:20 +0200

    Oh thats a miracle

--- main.py
+++ main.py
@@ -52,42 +52,59 @@ window = pyglet.window.Window(WIDHT, HEIGHT)
 
 t = 0
 
-drawing = []
-maxx, maxy = 0, 0
-with open("points10.txt") as f:
+# drawing = []
+# maxx, maxy = 0, 0
+drawing = np.array([[0, 0]])
+
+with open("points3.txt") as f:
     for line in f.readlines():
         if line != "":
             x, y = line.rstrip("\n").split(",")
-            maxx, maxy = max(maxx, float(x)), max(maxy, float(y))
-            drawing.append([float(x), float(y)])
+            drawing = np.vstack([drawing, [[float(x), float(y)]]])
 
-for i, d in enumerate(drawing):
-    drawing[i] = [d[0] / maxx, d[1] / maxy]
+            # drawing.append([float(x), float(y)])
+
+# print(max(drawing, axis=0))
+# for i, d in enumerate(drawing):
+#     drawing[i] = [d[0] / maxx, d[1] / maxy]
+
+# print(maxx, maxy)
+# print(drawing[1:].max(axis=0))
+drawing = (drawing[1:] - drawing[1:].max(axis=0) / 2) / drawing[1:].max(axis=0)
+# print(drawing[1:].max(axis=0))
 
 
 def drawFromTXT(t):
     # T Ranges from 0 to 1
     l = len(drawing)
-    index = floor(t*l)
-    return complex(drawing[index][0], drawing[index][1])
+    index = floor(t*l) - 1
+    # print(index, l)
+    # print(drawing[index])
+    # print()
+
+    return complex(drawing[index][0], -drawing[index][1])
 
 
 vectors = []
-for n in range(-2, 3):
+ns = 300
+for n in sorted(range(-ns//2, ns//2+1), key=abs):
     speed = n
 
-    re, err = quad(lambda t: ((e**complex(-2*pi*n*t))
-                              * drawFromTXT(t)).real, 0, 1)
-    img, err = quad(lambda t: ((e**complex(-2*pi*n*t))
-                               * drawFromTXT(t)).imag, 0, 1)
+    # re, err = quad(lambda t: ((e**complex(-2*pi*n*t))
+    #                           * drawFromTXT(t)).real, 0, 1)
+    # img, err = quad(lambda t: ((e**complex(-2*pi*n*t))
+    #                            * drawFromTXT(t)).imag, 0, 1)
+
+    c = sum([(e**complex(0, -2*pi*n*t))
+             * drawFromTXT(t) for t in np.linspace(0, 1, 1000)])
 
     # r = complex_quadrature(
     #     lambda t: (e**complex(-2*pi*n*t))*drawFromTXT(t), 0, 1)
-    vectors.append(Vector(speedm=speed, scale=1, c=complex(re, img)))
-    print(complex(re, img))
+    vectors.append(Vector(speedm=speed, scale=0.3, c=c))
+    # print(speed, c)
 
 points = np.array([[0, 0]])
-
+# print(points.shape)
 
 # vectors = [
 #     Vector(speedm=i, scale=150, c=complex(1/i, 0)*e**complex(0, random()*2*pi)) for i in range(1, 50)
@@ -109,9 +126,9 @@ def on_draw(dt):
 
         vector.draw()
 
-    # points = np.vstack([points, [[vectors[-1].x, vectors[-1].y]]])
-    # pyglet.graphics.draw(len(points)-1, pyglet.gl.GL_LINE_STRIP,
-    #                      ('v2f', (shift_coord(points[1:])).flatten()))
+    points = np.vstack([points, [[vectors[-1].x, vectors[-1].y]]])
+    pyglet.graphics.draw(len(points)-2, pyglet.gl.GL_LINE_STRIP,
+                         ('v2f', (shift_coord(points[2:])).flatten()))
 
 
 def shift_coord(points, offset=[WIDHT/2, HEIGHT/2]):
